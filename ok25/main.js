12a785dc192adc9be24679147568cefcc3e27223
Tomas <tomaslonnblad@gmail.com>
2025-06-30 13:23:40 +0200

    Update main.js

--- ok25/main.js
+++ ok25/main.js
@@ -51,7 +51,7 @@ spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
 const loader = new GLTFLoader().setPath('public/millennium_falcon2/');
-loader.load('scene.glb', (gltf) => {
+loader.load('scene.gltf', (gltf) => {
   console.log('loading model');
   const mesh = gltf.scene;
 
