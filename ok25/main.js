c7de5f5cd312fcd73ca9276af7c6ea25de58ae24
Tomas <tomaslonnblad@gmail.com>
2025-06-30 10:12:58 +0200

    Update main.js

--- ok25/main.js
+++ ok25/main.js
@@ -18,15 +18,15 @@ const scene = new THREE.Scene();
 
   // Set up camera
   const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.set(5, 15, 70);
+  camera.position.set(5, 5, 70);
 
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = true;
-controls.minDistance = 40;
-controls.maxDistance = 20;
+controls.minDistance = 20;
+controls.maxDistance = 10;
 controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = true;
