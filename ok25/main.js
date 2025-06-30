1bb9a926c072fe713f45b9a8dfe8ad6daf650b44
Tomas <tomaslonnblad@gmail.com>
2025-06-30 09:48:26 +0200

    Update main.js

--- ok25/main.js
+++ ok25/main.js
@@ -18,7 +18,7 @@ const scene = new THREE.Scene();
 
   // Set up camera
   const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.set(5, 15, 160);
+  camera.position.set(2, 15, 60);
 
 
 
