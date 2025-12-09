00c2e15940301e4cfff9cf7eb8bd6c445dc50761
Tomas <tomaslonnblad@gmail.com>
2025-12-09 12:15:13 +0100

    Update main.js

--- ok25/main.js
+++ ok25/main.js
@@ -17,23 +17,23 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
   // Set up camera
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.set(15, 15, 160);
+  const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(10, 10, 150);
 
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = true;
-controls.minDistance = 40;
-controls.maxDistance = 40;
-controls.minPolarAngle = 0.5;
+controls.minDistance = 190;
+controls.maxDistance = 150;
+controls.minPolarAngle = 0.8;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = true;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
-const groundGeometry = new THREE.PlaneGeometry(10, 10, 10, 32);
+const groundGeometry = new THREE.PlaneGeometry(100, 10, 10, 32);
 groundGeometry.rotateX(-Math.PI / 2);
 const groundMaterial = new THREE.MeshStandardMaterial({
   color: 0x555555,
@@ -44,10 +44,10 @@ groundMesh.castShadow = true;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 90000, 3000, 9.22, 3);
-spotLight.position.set(10, 10, 10);
+const spotLight = new THREE.SpotLight(0xffffff, 84000, 2000, 9.72, 2);
+spotLight.position.set(2, 100, 100);
 spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
+spotLight.shadow.bias = -0.01;
 scene.add(spotLight);
 
 const loader = new GLTFLoader().setPath('public/millennium_falcon/');
@@ -62,7 +62,7 @@ loader.load('scene.gltf', (gltf) => {
     }
   });
 
-  mesh.position.set(0, 0.02, -1);
+  mesh.position.set(0, 0.44, -1);
   scene.add(mesh);
 
   document.getElementById('').style.display = 'none';
